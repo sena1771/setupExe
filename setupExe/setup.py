55c45291ac2a665bdc318b1d3e094915963d63d3
sena1771 <64974096+sena1771@users.noreply.github.com>
2024-06-13 09:45:28 +0300

    Update setup.py
    multithreading has been added working synchronized for cancel and extract operation

--- setupExe/setup.py
+++ setupExe/setup.py
@@ -7,21 +7,32 @@ import sys
 import tkinter as tk
 import shutil
 from tkinter import ttk
+import threading
+import queue
+import time
 #import shutil
 
 
-ip_address = "127.0.0.1"
-port = "9271"
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 
 version = "0.1.4"
 
 folder_path = 'C:\\Example Archive Folder' #TODO: archive file name
-script_directory = os.path.dirname(os.path.abspath(__file__))
+
+if getattr(sys,'frozen',False):
+    import pyi_splash
+    script_directory =sys._MEIPASS
+else:
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+
 
 zip_path = os.path.join(script_directory,'Example_Archive_Folder.zip')
 
 icon_path = os.path.join(script_directory,'some-icon.ico')
+
+stop_extracting = threading.Event()
+update_queue = queue.Queue()
 
 def extract_files():
     if not os.path.exists(folder_path):
@@ -30,12 +41,14 @@ def extract_files():
     if os.path.exists(zip_path):
         with zipfile.ZipFile(zip_path,'r') as zp:
             total_files = len(zp.namelist())
-            progress_bar['maximum'] = total_files
+            root.after(0,lambda : progress_bar.config(maximum = total_files)) 
+            #progress_bar['maximum'] = total_files
             extracted_count = 0
             
             for file_index, file in enumerate(zp.namelist()):
                     
-                    
+                    if stop_extracting.is_set():
+                        break
                     file_path= os.path.join(folder_path,file)
                     directory = os.path.dirname(file_path)
                     #if len(file_path) >= 260 :
@@ -49,7 +62,9 @@ def extract_files():
                     if not os.path.exists(directory):
                         
                         os.makedirs(directory)
-                        current_file_label.config(text=f"New directory created : {directory}")
+                        root.after(0,lambda : current_file_label.config(text = f"New directory created : {directory}")) 
+                       
+                        #current_file_label.config(text=f"New directory created : {directory}")
 
 
                     if len(file_path) >= 260 :
@@ -60,85 +75,152 @@ def extract_files():
                             
                             shutil.copyfileobj(file_data,output_file)
                             extracted_count += 1
+
+                            root.update_idletasks()
                             
                     except Exception as ex:
-                            current_file_label.config(text =f"Failed to extract {file}: {ex}")
-                       
+                            #current_file_label.config(text =f"Failed to extract {file}: {ex}")
+                            root.after(0,lambda :current_file_label.config(text= f"Failed to extract {file} "))
                       
                     #if not os.path.exists(file_path) or os.path.getsize(file_path)!= zp.getinfo(file).file_size:
                          #print("3")
                          #zp.extract(file,folder_path)
                         
-                       
+                    
+                    root.after(0,lambda : extracting_file_label.config(text = f"Extracting: {file}"))                   
+                    root.after(0,lambda : progress_bar.config(value = file_index + 1))
                      
                     current_file_label.config(text=f"Extracting : {file}")
                     
-                    progress_bar['value'] = file_index + 1
+                    #progress_bar['value'] = file_index + 1
                     
                     root.update_idletasks()
                    
 
-            info_text = f" {extracted_count} files downloaded or updated."     
+            #info_text = f" {extracted_count} files downloaded or updated."     
                     
-        #info_label.config(text="New version downloaded successfully.")
-        info_label.config(state ='normal')
+    if not stop_extracting.is_set(): 
+        
+        info_label.config(state='normal')
         info_label.delete(1.0,tk.END)
-        info_label.insert(tk.END, "New version downloaded successfully.")
-        info_label.config(state = 'disabled')
-            #print("Extraction completed")
-        download_button.pack_forget()
+        info_label.insert(tk.END,"New version downloaded successfully")
+        info_label.config(state='disabled')
         progress_bar.pack_forget()
-        current_file_label.pack_forget()
-        ok_button.pack(side=tk.LEFT, padx=20, pady=20) 
+        extracting_file_label.pack_forget()
+        ok_button.pack(side=tk.LEFT, pady=10,padx=10)
+        
         cancel_button.pack_forget()
-    else:
-         #print(" Archive is not found")
-         info_label.config(state = 'normal')
-         info_label.delete(1.0,tk.END)
-         #info_label.config(text="Download operation is failed")
-         info_label.insert(tk.END, "Download failed")
-         info_label.config(state = 'disabled')
-         download_button.pack_forget()
-         progress_bar.pack_forget()
-         current_file_label.pack_forget()
+                  
+def simulate_extraction(file):
 
-def download_and_update():
-    download_button.pack_forget()
-    progress_bar['value'] = 0
-    progress_bar.pack(side= tk.TOP, fill = tk.X, padx=20, pady=5)
-    current_file_label.pack(side = tk.TOP, fill = tk.X, padx = 20)
-    root.after(100,extract_files)
+    for _ in range(10):
+        if stop_extracting.is_set():
+            return
+        root.after(0,extractinComplete)
+        #root.update_idletasks()
+def start_extracting():
+     
+    
+     stop_extracting.clear()
+     progress_bar.pack()
+     extracting_file_label.pack()
+     
+
+     threading.Thread(target=extract_files).start()
+     download_button.pack_forget()
+    
+def extractinComplete():
+     
+     info_label.config(state='normal')
+     info_label.delete(1.0,tk.END)
+     info_label.insert(tk.END,"New version downloaded successfully")
+     info_label.config(state='disabled')
+     download_button.pack_forget()
+     progress_bar.pack_forget()            
+     extracting_file_label.pack_forget()
+     ok_button.pack(side=tk.LEFT, pady=10,padx=10)
+     progress_bar['value'] = progress_bar['maximum']
+     cancel_button.pack_forget()
+   
+def cancel_extracting() :
+     stop_extracting.set()
+     
+     info_label.config(state = 'normal')
+     info_label.delete(1.0,tk.END)
+     info_label.insert(tk.END,"Extraction cancelled")
+     info_label.config(state = 'disabled')
+     ok_button.pack(side=tk.LEFT, pady=10,padx=10)
+       
+     progress_bar.pack_forget()
+     download_button.pack_forget()
+     cancel_button.pack_forget()
+     current_file_label.pack_forget()
+     extracting_file_label.pack_forget()
+    
 def close_screen():
-    root.destroy()
+    #thread close
+    stop_extracting.set()
+    info_label.config(state = 'normal')
+    info_label.delete(1.0,tk.END)
+    info_label.insert(tk.END,"Extraction cancelled")
+    info_label.config(state = 'disabled')
+    progress_bar.pack_forget()
+    download_button.pack_forget()
+    cancel_button.pack_forget()
+    current_file_label.pack_forget()
+    extracting_file_label.pack_forget()
+    root.destroy()    
 
-  
+
+
 root= tk.Tk()
+root.resizable(width=False,height=False)
 root.iconbitmap(icon_path)
 root.eval('tk::PlaceWindow . center') #centers window
-root.geometry('450x300')
+root.geometry('550x400')
 root.title("IPP Platform Configuration Tool")
-info_text = "Archive--- Version:" +version+" "+"will be downloaded.\nNew Updates;\n- Configuration\n-HSN Configuration\n-Report File Creation"
+
+root.configure(background='steel blue')
+
+
+title_label = tk.Label(root,text="Installer",width=40,font=("Ink Free",25,'bold'),bg="steel blue",fg="gold")
+title_label.pack(side =tk.TOP,padx=30)
+
+info_text = "IPP Platform Configuration Tool --- Version:" +version+" "+"will be downloaded.\nNew Updates;\n-DTN Interrack Switch Configuration\n-HSN Configuration\n-Report File Creation" 
 info_label = tk.Text(root,height= 10, width = 50)
 info_label.insert(tk.END, info_text)
 info_label.pack(pady=10,padx=10)
 info_label.config(state = 'disabled')
 
+
 current_file_label = tk.Label(root,text = "Waiting to start...", anchor ="w")
 frame_buttons = tk.Frame(root)
 frame_buttons.pack(side = tk.BOTTOM, fill = tk.X)
 
-download_button = tk.Button(frame_buttons,text ="Download/Update", command= download_and_update)
+download_button = tk.Button(frame_buttons,text ="Install/Update", command= start_extracting,bg="steel blue",fg="white",width=23)
 download_button.pack(side=tk.RIGHT,padx=20, pady=20)
 
-cancel_button = tk.Button(frame_buttons,text= "Cancel", command = close_screen)
+cancel_button = tk.Button(frame_buttons,text= "Cancel", command = cancel_extracting,bg="steel blue",fg="white",width=25)
 cancel_button.pack(side=tk.LEFT,padx =20,pady=20)
 
+ok_button = tk.Button(frame_buttons,text="OK", command = close_screen,width=25,bg="steel blue",fg="white")
 
-ok_button = tk.Button(frame_buttons, text="OK", command=close_screen) 
+progress_label_frame = tk.Frame(root,bg="steel blue")
+progress_label_frame.pack(pady=5,expand=True)
 
-progress_bar = ttk.Progressbar(root,orient='horizontal', mode= 'determinate', length=300)
 
+progress_bar = ttk.Progressbar(progress_label_frame,orient='horizontal', mode= 'determinate', length=300)
+
+
+extracting_file_label = tk.Label(progress_label_frame,text="",fg="white",bg="steel blue")
+
+
+if getattr(sys,'frozen',False):
+     pyi_splash.close()
+  
+    
 root.mainloop()
+
 
 
 
